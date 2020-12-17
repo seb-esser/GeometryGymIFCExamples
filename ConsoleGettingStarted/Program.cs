cbf65ac7a69acd17011b43952ff8a4f319ab936a
Sebastian_Esser <sebastian.esser@tum.de>
2020-12-17 09:30:33 +0100

    Gettingstarted

---
+++ ConsoleGettingStarted/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryGym.Ifc;
+
+namespace ConsoleGettingStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var database = new DatabaseIfc(ModelView.Ifc4X3NotAssigned);
+            database.Factory.ApplicationFullName = "GeometryGymIfc Getting started";
+
+            var element = new IfcBuiltElement(database);
+            element.AddComment("some comment");
+
+            database.WriteFile("myIfcFile.ifc");
+
+        }
+    }
+}
