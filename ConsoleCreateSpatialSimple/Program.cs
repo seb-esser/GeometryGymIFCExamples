42b64870bf9de62b60c40aaeeda2091a744dfb53
Sebastian_Esser <sebastian.esser@tum.de>
2021-06-08 11:57:21 +0200

    new sample

---
+++ ConsoleCreateSpatialSimple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GeometryGym.Ifc;
+
+namespace ConsoleCreateSpatialSimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create database 
+            var database = new DatabaseIfc(ModelView.Ifc4NotAssigned);
+
+            // create IfcSite instance
+            var site = new IfcSite(database, "Site entity");
+
+            // create top-most spatial structure element IfcProject, set units and assign facility to project
+            var project = new IfcProject(site, "Project entity");
+
+            database.WriteFile("spatial.ifc");
+
+        }
+    }
+}
